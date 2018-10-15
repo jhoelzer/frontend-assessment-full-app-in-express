8424874660b7abef52a69618a72108bd62f6806b
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-10-15 17:11:13 -0400

    i think it works

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
-const app = express()
-const publicFolderPath = path.join(__dirname, "public")
+const app = express();
+const publicFolderPath = path.join(__dirname, "public");
 
-app.use(express.json())
-app.use(express.static(publicFolderPath))
+app.use(express.json());
+app.use(express.static(publicFolderPath));
 
-const users = []
+const users = [];
 
 // add POST request listener here
+app.post("/api/user", function (req, res) {
 
-app.listen(3000);
+    if (users.find(user => user.username === req.body.username)) {
+        res.status(409);
+        res.send(new Error("Username taken, choose new username"));
+    } else {
+        req.body.id = Math.floor(Math.random() * 1000);
+        users.push(req.body);
+        res.status(201);
+        res.send(req.body);
+    }
+    console.log(users);
+})
+
+app.listen(3000, () => console.log("running on port 3000"));
--- public/index.js
+++ public/index.js
@@ -1,2 +1,29 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+
+userCreateForm.addEventListener("submit", submitForm)
+
+function submitForm (event) {
+    event.preventDefault();
+
+    let user = {
+        email: document.getElementById("email").value,
+        username: document.getElementById("username").value,
+        firstName: document.getElementById("first").value,
+        lastName: document.getElementById("last").value
+    }
+
+    console.log("User: " + user)
+
+    const post = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    };
+
+    fetch("/api/user", post)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
